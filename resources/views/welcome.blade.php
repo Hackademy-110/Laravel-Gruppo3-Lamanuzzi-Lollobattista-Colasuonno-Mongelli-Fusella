97d4955dcdff729b281a40885e14455b0fdd67c9
Matteo <lamanuzzi.matteo@gmail.com>
2023-12-21 10:39:17 +0100

    setup

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        @vite (['resources\css\app.css', 'resources\js\app.js'])
+        <title>Laravel Gruppo 3</title>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <section class="row bg-info">
+                <h1 class="text-danger text-center">Buonsalve</h1>
+            </section>
+        </div>
+    
+    </body>
+</html>
